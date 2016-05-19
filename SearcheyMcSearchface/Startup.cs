ca11d21f35a3df9b18d2247912ede646d200ebd3
gkaradjov <gogokaradjov@gmail.com>
2016-05-19 12:42:02 +0300

    initial commit

---
+++ SearcheyMcSearchface/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearcheyMcSearchface.Startup))]
+namespace SearcheyMcSearchface
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
