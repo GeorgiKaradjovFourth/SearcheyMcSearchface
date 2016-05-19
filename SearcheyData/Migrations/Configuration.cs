5352071f04e949f75792716f755deaea29ae4f91
Tsonko Tsonev <Tsonko.Tsonev@fourth.com>
2016-05-19 16:13:39 +0300

    Data configuration fixes

---
+++ SearcheyData/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace SearcheyData.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SearcheyData.SearcheyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "SearcheyData.SearcheyContext";
+        }
+
+        protected override void Seed(SearcheyData.SearcheyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- SearcheyData/SearcheyContext.cs
+++ SearcheyData/SearcheyContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,12 @@ namespace SearcheyData
     {
         public SearcheyContext() : base("DefaultConnection")
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
         }
 
         public DbSet<Document> Documents { get; set; }
