9cc9bfd3a9ba39f59c332aba0db35bcf730ec64b
Tsonko Tsonev <Tsonko.Tsonev@fourth.com>
2016-05-20 12:58:01 +0300

    Progress with tags

--- SearcheyMcSearchface/Controllers/SearchController.cs
+++ SearcheyMcSearchface/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SearcheyData;
 using SearcheyData.Entities;
 using SearcheyMcSearchface.Models;
 
@@ -19,11 +20,14 @@ namespace SearcheyMcSearchface.Controllers
 
         private const int SHOW_FIRST_CHARACTERS = 255;
         private const int SHOW_FIRST_DOCUMENTS_COUNT = 20;
+        private const int SHOW_FIRST_TAGS_COUNT = 5;
 
         // GET: Search/Text/
         public ActionResult Search(string text)
         {
             var result = LuceneSearch.Search(text);
+
+            var context = new SearcheyContext();
 
             SearchViewModel model = new SearchViewModel();
             model.Text = text;
@@ -33,7 +37,7 @@ namespace SearcheyMcSearchface.Controllers
                 Text = s.Text.Length > SHOW_FIRST_CHARACTERS ? s.Text.Substring(0, SHOW_FIRST_CHARACTERS) : s.Text,
                 Header = s.Header,
                 Source = s.Source,
-                Tags = new List<Tag>()
+                Tags = context.Documents.FirstOrDefault(d => d.Id == s.Id).Tags.Take(SHOW_FIRST_TAGS_COUNT).ToList()
             }).Take(SHOW_FIRST_DOCUMENTS_COUNT).ToList();
 
             return PartialView("_Search", model);
